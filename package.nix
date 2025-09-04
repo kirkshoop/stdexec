86e126a55ee089b1a3e46516e672535e30af8de8
Kirk Shoop <kirk.shoop@gmail.com>
2025-09-04 06:34:15 -0700

    WIP - enable nix build

--- package.nix
+++ package.nix
@@ -6,6 +6,8 @@
 { lib
 , stdenv
 , cmake
+, extra-cmake-modules
+, cpm-cmake
 , gtest
 , ninja
 , nixpkgs-fmt
@@ -17,12 +19,16 @@
 , enableExamples ? true
   # Documents are by default disabled, but the caller can enable them
 , enableDocuments ? false
+, doxygen
+, sphinx
   # Extra type checks are by default disabled, but the caller can enable them
 , enableExtraTypeChecks ? false
   # TBB is by default disabled, but the caller can enable 
-, enableTBB ? false
+, enableTBB ? true
+, tbb
   # IO_URING is by default detected, but the caller can force-enable 
 , enableIoUring ? false
+, liburing
   # CUDA is by default detected, but the caller can force-enable 
 , enableCUDA ? false
 }:
@@ -38,15 +44,23 @@ stdenv.mkDerivation {
     # C++ Compiler is already part of stdenv
     stdenv
     cmake
+    extra-cmake-modules
+    cpm-cmake
     gtest
     ninja
     nixpkgs-fmt
-  ];
+  ]
+    ++ lib.optional (enableDocuments) doxygen
+    ++ lib.optional (enableDocuments) sphinx
+    ++ lib.optional (enableTBB) tbb
+    ++ lib.optional (enableIoUring) liburing
+  ;
 
   # good source filtering is important for caching of builds.
   # It's easier when subprojects have their own distinct subfolders.
   src = lib.sourceByRegex ./. [
     "^include.*"
+    "^src.*"
     "^test.*"
     "^examples.*"
     "CMakeLists.txt"
@@ -66,8 +80,11 @@ stdenv.mkDerivation {
   # Distinguishing between `nativeBuildInputs` (runnable on the host
   # at compile time) and normal `buildInputs` (runnable on target
   # platform at run time) is an important preparation for cross-compilation.
-  nativeBuildInputs = [ cmake ninja ];
-  buildInputs = [ ];
+  nativeBuildInputs = [ cmake extra-cmake-modules cpm-cmake ninja ];
+  buildInputs = [ ]
+    ++ lib.optional (enableTBB) tbb
+    ++ lib.optional (enableIoUring) liburing
+  ;
   checkInputs = [ gtest ];
 
   # Instruct the build process to run tests.
